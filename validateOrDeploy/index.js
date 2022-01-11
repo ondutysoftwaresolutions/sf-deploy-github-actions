b85590e9a283c2039f87251c1e5030b08dbcc6b1
Pablo Martinez <pablo@beintelligent.nz>
2022-01-12 08:45:39 +1300

    fix the issue with failing tests

--- validateOrDeploy/index.js
+++ validateOrDeploy/index.js
@@ -50,8 +50,8 @@ const run = async () => {
     // parsed the result
     const parsedResult = JSON.parse(result);
     // if it was a success (status = 0)
-    if ((parsedResult.status === 0 && configuration.orgType === constants_1.OrgType.SANDBOX) ||
-        configuration.orgType === constants_1.OrgType.PRODUCTION) {
+    if ((parsedResult.status === 0 && configuration.orgType === constants_1.OrgType.SCRATCH) ||
+        configuration.orgType !== constants_1.OrgType.SCRATCH) {
         // if it was a deployment, check the tests results if need it.
         // if it was a validation, process the results and return the job id
         if (configuration.deploy) {
@@ -74,7 +74,6 @@ const run = async () => {
     else {
         core_1.setFailed('The Deployment of the package failed');
         core_1.info('\nCOMPONENTS WITH ERRORS: \n');
-        core_1.info('\nRESULT: \n' + JSON.stringify(parsedResult));
         processValidationResult_1.printDeploymentErrorsResult(parsedResult.result);
     }
 };
